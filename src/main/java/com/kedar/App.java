06ea10b32905280cd668cc8e16c0083778fc39c1
i351252 <kedar.ratnaparkhi@sap.com>
2021-09-22 18:40:48 +0530

    first push

---
+++ src/main/java/com/kedar/App.java
@@ -0,0 +1,13 @@
+package com.kedar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
