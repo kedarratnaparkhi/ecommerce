706e8b97e4543193fd2aa423d0eb15a7b8dd7a68
kedarratnaparkhi <kedar.ratnaparkhi.dev@gmail.com>
2021-10-08 10:44:13 +0530

    Added Address Repository Class

--- src/main/java/com/kedar/ecommerce/repo/AddressRepository.java
+++ src/main/java/com/kedar/ecommerce/repo/AddressRepository.java
@@ -4,7 +4,11 @@ import com.kedar.ecommerce.domain.Address;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AddressRepository extends CrudRepository<Address, Long> {
+
+    public List<Address> findByPostCode(String postCode);
 
 }
