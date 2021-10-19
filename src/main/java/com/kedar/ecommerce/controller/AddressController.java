96cf9a17a3adbc0e7739eea3d7a8e11f570469e5
kedarratnaparkhi <kedar.ratnaparkhi.dev@gmail.com>
2021-10-19 19:02:46 +0530

    Implemented Address Controller with basic Endpoints

---
+++ src/main/java/com/kedar/ecommerce/controller/AddressController.java
@@ -0,0 +1,41 @@
+package com.kedar.ecommerce.controller;
+
+import com.google.gson.Gson;
+import com.kedar.ecommerce.Exception.DataValidationException;
+import com.kedar.ecommerce.domain.Address;
+import com.kedar.ecommerce.service.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("address")
+public class AddressController {
+
+    private AddressService addressService;
+
+    @Autowired
+    AddressController(AddressService addressService){
+        this.addressService = addressService;
+    }
+
+    Gson gson = new Gson();
+
+    @GetMapping(path = "/")
+    public List<Address> getAllAddress(){
+        return addressService.findAll();
+    }
+
+    @GetMapping(path = "/{postcode}")
+    public Address getAddressByPostCode(@PathVariable String postcode){
+        return addressService.findByPostCode(postcode).get(0);
+    }
+
+
+    @PostMapping("/add")
+    public Address addAddress(@RequestBody String requestBody) throws DataValidationException {
+        Address addressToBeSaved = gson.fromJson(requestBody, Address.class);
+        return addressService.save(addressToBeSaved);
+    }
+}
