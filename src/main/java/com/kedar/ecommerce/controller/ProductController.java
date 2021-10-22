2b91dac167b60a3d4ad51ddadb233f6a4ebfc3ef
kedarratnaparkhi <kedar.ratnaparkhi.dev@gmail.com>
2021-10-22 11:43:47 +0530

    Implemented Product Controller with basic Endpoints

--- src/main/java/com/kedar/ecommerce/controller/ProductController.java
+++ src/main/java/com/kedar/ecommerce/controller/ProductController.java
@@ -1,4 +1,51 @@
 package com.kedar.ecommerce.controller;
 
+import com.google.gson.Gson;
+import com.kedar.ecommerce.Exception.DataValidationException;
+import com.kedar.ecommerce.domain.Product;
+import com.kedar.ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("product")
 public class ProductController {
+
+    private ProductService productService;
+
+    @Autowired
+    ProductController(ProductService productService){
+        this.productService = productService;
+    }
+
+    Gson gson = new Gson();
+
+    @GetMapping(path = "/")
+    public List<Product> getAllProducts(){
+        return productService.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Product getproductByPostCode(@PathVariable Long id){
+        return productService.findById(id);
+    }
+
+
+    @PostMapping("/add")
+    public Product addproduct(@RequestBody String requestBody) throws DataValidationException {
+        Product productToBeSaved = gson.fromJson(requestBody, Product.class);
+        return productService.saveProduct(productToBeSaved);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Boolean deleteProduct(@PathVariable Long id) throws DataValidationException {
+        try{
+            productService.deleteProduct(id);
+            return true;
+        }catch(Exception ex){
+            return false;
+        }
+    }
 }
