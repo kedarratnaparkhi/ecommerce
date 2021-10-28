f4b464bc52f9ca7d0934211e9c761323f62fdf67
kedarratnaparkhi <kedar.ratnaparkhi.dev@gmail.com>
2021-10-28 14:56:27 +0530

    Implement Basic Spring Security

---
+++ src/main/java/com/kedar/ecommerce/security/BasicWebSecurity.java
@@ -0,0 +1,41 @@
+package com.kedar.ecommerce.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class BasicWebSecurity extends WebSecurityConfigurerAdapter {
+
+    //This is the AUTHENTICATION part
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("kedar")
+                .password("kedar")
+                .roles("USER", "EXTRA_ROLE")
+                .and()
+                .withUser("admin")
+                .password("admin")
+                .roles("ADMIN");
+    }
+
+    //This is the AUTHORIZATION part
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/customer/**").hasRole("ADMIN")
+                .antMatchers("/address/**").hasRole("USER")
+                .antMatchers("/").permitAll()
+                .and().httpBasic();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
